aa72af875a08db99821f60c3b225d0f713e993b3
StitchMl <matteo.lagioia@gmail.com>
2026-03-20 18:11:53 +0100

    Bump version name to 4.0
    - Update `versionName` from 3.5 to 4.0 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "3.5"
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GEMINI_API_KEY", "\"$geminiApiKey\"")
